ce9d56cff3b13db6b432cb674fc970b8e2fe1daf
luisplduarte <luis.pedro.duarte.24@gmail.com>
2024-09-24 17:39:27 +0100

    Changed answer template to improve answer prompt

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const STANDALONE_TEMPLATE = `Given some conversation history (if any) and a ques
 
 // This answer template will tell AI how to respont to user question. We give the context (knowledge), conversation history
 //as well as the user question to the prompt so it has more information to create the response
-const ANSWER_TEMPLATE = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided and the conversation history provided. Try to find the answer in the context. If the answer is not given in the context, find the answer in the conversation history if possible. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
+const ANSWER_TEMPLATE = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided and the conversation history provided. Try to find the answer in the context. If the answer is not given in the context, find the answer in the conversation history if possible and reply using you own words. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
   context: {context}
   conversation history: {conversation_history}
   question: {question}
@@ -51,11 +51,9 @@ const conversationHitory = [];
 
 app.post('/chat', async (req, res) => {
     try {
-        //const { userQuestion, history = [] } = req.body
         const { userQuestion } = req.body
         if(!userQuestion) throw new Error(`Missing user question!`);
 
-        
         // Append current user question to history
         conversationHitory.push({ role: 'user', content: userQuestion }); // History structure -> [{ role: "user", content: "message" }, { role: "bot", content: "response" }, ...]
         const formattedConversationHistory = conversationHitory.map(h => `${h.role}: ${h.content}`).join('\n');
